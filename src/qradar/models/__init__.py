69a0a450eb3db05f063dcf773aeb307cda4efda8
thisisnottheuseryouarelookingfor <vangoethempieter@gmail.com>
2021-03-17 08:33:16 +0000

    explicit imports on qradar.model

--- src/qradar/models/__init__.py
+++ src/qradar/models/__init__.py
@@ -1,12 +1,5 @@
-from inspect import isclass
-from pkgutil import iter_modules
-from pathlib import Path
-from importlib import import_module
-
-package_dir = Path(__file__).resolve().parent
-for (_, module_name, _) in iter_modules([package_dir]):
-    module = import_module(f"{__name__}.{module_name}")
-    for attribute_name in dir(module):
-        attribute = getattr(module, attribute_name)
-        if isclass(attribute):
-            globals()[attribute_name] = attribute
+from .domain import Domain
+from .login_attempt import LoginAttempt
+from .logsource import Logsource
+from .logsourcegroup import LogsourceGroup
+from .saved_search_group import SavedSearchGroup
