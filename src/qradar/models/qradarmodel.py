822a91c0c10133c1d9ae7a8f63ca269265c84321
thisisnottheuseryouarelookingfor <vangoethempieter@gmail.com>
2021-04-13 08:05:27 +0000

    revert to old from_json, is cleaner

--- src/qradar/models/qradarmodel.py
+++ src/qradar/models/qradarmodel.py
@@ -1,11 +1,11 @@
 class QRadarModel:
     @classmethod
     def from_json(cls, json):
-        if isinstance(json, list) and len(json) == 1:
+        if isinstance(json, dict):
             obj = cls()
             for att in vars(obj):
                 if att is not None:
-                    setattr(obj, att, json[0].get(att, None))
+                    setattr(obj, att, json.get(att, None))
             return obj
         if isinstance(json, list):
             listofitems = []
